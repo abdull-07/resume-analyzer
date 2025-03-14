b7f96abbdffb864e12d8c5159cf21a69347245d6
abdull-07 <personal.muhammadabdullah@gmail.com>
2025-03-14 16:09:20 +0500

    Initialz the React App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+ <h1 className='text-3xl font-bold underline text-cyab-500 bg-blue-600'>Wellcome</h1>
+    </>
+  )
+}
+
+export default App
