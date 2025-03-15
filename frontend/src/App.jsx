d795b98a0c6b714ba8199e1fd7e7e90853e30f79
abdull-07 <personal.muhammadabdullah@gmail.com>
2025-03-15 12:47:51 +0500

    Add Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import './App.css';
 
 function App() {
-
-
   return (
     <>
- <h1 className='text-3xl font-bold underline text-cyab-500 bg-blue-600'>Wellcome</h1>
+      <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+        <h1 className="text-3xl font-bold">WAres</h1>
+        <nav>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </nav>
+      </header>
+
+      <main className="p-6">
+        <h2 className="text-xl text-blue-500">This is the home page.</h2>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
